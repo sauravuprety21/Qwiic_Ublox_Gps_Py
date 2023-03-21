5759b9ea93dbd63aecf4832bb95cc55f88db4eba
sauravuprety21 <saurav.uprety1@ucalgary.ca>
2023-03-21 00:48:37 -0600

    init selected nmea messages example

---
+++ examples/get_selcted_nmea.py
@@ -0,0 +1,30 @@
+import pynmea2
+
+import sys
+
+sys.path.append('..')
+
+from ublox_gps.ublox_gps import UbloxGps, NMEACfg
+
+import serial
+
+port = '/dev/serial0'
+ser = serial.Serial(port, baudrate=9600, timeout=1)
+gps = UbloxGps(ser)
+
+
+def main():
+    NMEACfg.disable_all(gps)
+    NMEACfg.enable_msg(gps, 'GSV', 1)
+    try:
+        while 1:
+            try:
+                line = ser.readline().decode('utf-8')
+                msg = pynmea2.parse(line)
+                print(repr(msg))
+            except (ValueError, IOError) as err:
+                print(err)
+    finally:
+        ser.close()
+
+if __name__=="__main__": main()
