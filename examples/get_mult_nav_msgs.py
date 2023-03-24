374d54db9c14d59bb176550c399d150c8feae1be
sauravuprety21 <saurav.uprety1@ucalgary.ca>
2023-03-24 09:50:00 -0600

    RENAMED mult_prtcl to mult_nav

--- examples/get_mult_nav_msgs.py
+++ examples/get_mult_nav_msgs.py
@@ -6,27 +6,24 @@ import pynmea2
 sys.path.append('..')
 from ublox_gps.ublox_gps import UbloxGps, NMEACfg, NAV_MSGS
 from ublox_gps.sparkfun_predefines import NAV_CLS
+from ublox_gps.core import Parser
 
 port = '/dev/serial0'
 ser = serial.Serial(port, baudrate=38400, timeout=1)
+
 gps = UbloxGps(ser)
 
 
 def main():
-    NMEACfg.disable_all(gps)
-    NMEACfg.enable_msg(gps, 'GSA', 1)
-
+    parse_tool = Parser([NAV_CLS])
     try:
         while 1:
             try:
-                line = ser.readline().decode('utf-8')
-                msg = pynmea2.parse(line)
-                print(repr(msg))
-
-                for ubx_msg in ['PVT','COV']:
+                
+                for ubx_msg in ['PVT','COV','SAT']:
                     gps.send_message(NAV_CLS, NAV_MSGS.get(ubx_msg))
 
-                for ubx_msg in ['PVT','COV']:
+                for ubx_msg in ['PVT','SAT','COV']:
                     cls_name, msg_name, payload = parse_tool.receive_from(gps.hard_port)
                     print(ubx_msg, payload)              
 
